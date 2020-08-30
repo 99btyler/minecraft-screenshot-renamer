e5b8bef323ae129f697bbbfbe350c48d58aa4138
Tyler Beringer <99tylerberinger@gmail.com>
2020-08-29 21:28:10 -0400

    Fixed unordered/vanishing screenshots

--- Main.py
+++ Main.py
@@ -17,17 +17,20 @@ for file in os.listdir(source_folder):
 		datesplit = date.split("-")
 		d_year = datesplit[0]
 		d_month = datesplit[1].strip("0")
-		d_day = datesplit[2]
+		d_day = datesplit[2].strip("0")
 		better_date = f"{d_month}.{d_day}.{d_year}"
 
 		time = s_namesplit[1]
 		timesplit = time.split(".")
-		t_hour = timesplit[0] if int(timesplit[0]) < 12 else str(int(timesplit[0]) - 12)
+		t_hour = timesplit[0]
 		t_minute = timesplit[1]
-		t_period = "AM" if int(timesplit[0]) < 12 else "PM"
-		better_time = f"{t_hour}.{t_minute} {t_period}"
+		t_second = timesplit[2]
+		t_hourconverted = t_hour if (int(t_hour) > 0 and int(t_hour) <= 12) else abs(int(t_hour) - 12)
+		t_period = "AM" if int(t_hour) < 12 else "PM"
+		better_time = f"{t_hour}.{t_minute}.{t_second} ({t_hourconverted}.{t_minute} {t_period})"
 
 		s_name_2 = f"{better_date} @ {better_time}"
 
 		screenshot.save(f"{new_folder}/{s_name_2}{s_extension}")
-		print(f"Renamed '{s_name}' to '{s_name_2}'")
+		print(f"Renamed {s_name} to {s_name_2}")
+
